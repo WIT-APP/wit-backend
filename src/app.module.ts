5bf2603d65db7790f2b0b60fcd7b9930199e236e
tito <jfctito@gmail.com>
2023-09-08 17:13:43 +0200

    Connecting database, creating dummy entity user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from "./app.service";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import databaseConfig from "./config/database.config";
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
 	imports: [ConfigModule.forRoot({
@@ -14,7 +15,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 		imports: [ConfigModule],
 		inject: [ConfigService],
 		useFactory: (configService: ConfigService) => configService.get('db'),
-	  }),],
+	  }),
+	  UserModule,],
 	controllers: [AppController],
 	providers: [AppService],
 })
---
+++ src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dtos';
+
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+}
