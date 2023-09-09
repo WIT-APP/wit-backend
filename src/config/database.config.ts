87add7030a099c4353c1efc237de3c7f4076a38e
tito <jfctito@gmail.com>
2023-09-09 17:36:36 +0200

    docker compose ready and working

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,10 +4,9 @@ export default registerAs('db', () => ({
   type: 'mysql',
   entities: ['dist/**/*.entity.js'],
   synchronize: true,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  logging: "all"
+  host: 'mysql',
+  port: '3306',
+  username: 'root',
+  database: 'wit',
+  logging: 'all',
 }));
