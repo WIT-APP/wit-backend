8a907a49a48faf93cd9272103ecd79d510326651
Federico Javier Martino <federicojaviermartino@gmail.com>
2023-09-12 11:15:15 +0200

    create-applicant-dto

--- src/applicant/dto/create-applicant.dto.ts
+++ src/applicant/dto/create-applicant.dto.ts
@@ -1,9 +1,25 @@
 export class CreateApplicantDto {
-
     name: string;
     email: string;
     password: string;
-    residence: string;
-    languages: string;
-    Age: Number;
+    phoneNumber: number;
+    gender: string;
+    birthdate: number;
+    idNumber: string;
+    residenceAdress: string;
+    residenceCity: string;
+    residenceProvince: string;
+    zip: string;
+    programmeChoose: string;
+    spainPermission: string;
+    colectiveGroup: string;
+    educationLevel: string;
+    higherEducationTitle: string;
+    professionalSituation: string;
+    weeklyCommitment: string;
+    internetAccess: string;
+    eLearning: boolean;
+    reasons: string;
+    programmeFound: string;
+    extraInformation: string;
 }
