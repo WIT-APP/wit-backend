40afdcc0fbc7e5246cbb9ba63e0c5316f04aa30e
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-09-13 13:20:41 +0200

    enum names changed to minuscula

--- src/applicant/entities/applicant.enums.ts
+++ src/applicant/entities/applicant.enums.ts
@@ -1,79 +1,79 @@
 
 export enum TipoGenero {
-    Hombre = "Hombre",
-    Mujer = "Mujer",
-    Otro = "Otro",
-    Ninguno = "Prefiero no identificarme"
+    hombre = "Hombre",
+    mujer = "Mujer",
+    otro = "Otro",
+    ninguno = "Prefiero no identificarme"
 }
 
 export enum TipoId{
     DNI ="DNI",
     NIE ="NIE",
-    Otro = "Otro"
+    otro = "Otro"
 }
 
 export enum TipoProgramaDeseado {
     ITSupport = "Soporte de Tecnologías de la Información",
-    TechAutomation = "Automatización de Tecnologías de la Información con Python"
+    techAutomation = "Automatización de Tecnologías de la Información con Python"
 }
 
 export enum TipoPermiso {
-    Trabajo = "Permiso de residencia y trabajo",
-    Estudios = "Permiso de residencia y estudios",
-    Ninguno = "No dispongo de permiso (No es un problema para tener acceso al curso)",
-    Otro = "Otro"
+    trabajo = "Permiso de residencia y trabajo",
+    estudios = "Permiso de residencia y estudios",
+    ninguno = "No dispongo de permiso (No es un problema para tener acceso al curso)",
+    otro = "Otro"
 }
 
 export enum TipoColectivo {
-    Mujer = "Mujer en situación de vulnerabilidad",
-    MinoriasEtnicas = "Minorías étnicas",
-    Inmigrante = "Inmigrante o refugiada/o",
-    Joven = "Joven sin titulación y sin empleo",
-    Desempleado = "Desempleada/o de larga duración o debido a la crisis del Covid-19",
-    Edad50 = "Grupo de edad > 50 años",
-    Monoparental = "Cabeza de familia monoparental",
+    mujer = "Mujer en situación de vulnerabilidad",
+    minorias = "Minorías étnicas",
+    inmigrante = "Inmigrante o refugiada/o",
+    joven = "Joven sin titulación y sin empleo",
+    desempleado = "Desempleada/o de larga duración o debido a la crisis del Covid-19",
+    edad50 = "Grupo de edad > 50 años",
+    monoparental = "Cabeza de familia monoparental",
     Ninguno = "No me identifico con ninguna de las opciones anteriores"
 }
 
 export enum TipoEducacion {
-    SinEstudio = "Sin estudios o estudios primarios sin finalizar",
-    Primarios = "Estudios primarios",
-    Secundarios = "Estudios secundarios",
-    Profesional = "Formación profesional",
-    Universitarios = "Estudios universitarios",
-    Otro = "Otro"
+    sinEstudio = "Sin estudios o estudios primarios sin finalizar",
+    primarios = "Estudios primarios",
+    secundarios = "Estudios secundarios",
+    profesional = "Formación profesional",
+    universitarios = "Estudios universitarios",
+    otro = "Otro"
 }
 
 export enum TipoSituacionProfesional {
-    SinIngresos = "Desempleada/o sin ingresos",
-    Subsidio = "Desempleada/o con subsidio por desempleo",
-    TiempoCompleto = "Empleada/o a tiempo completo",
-    TiempoParcial = "Empleada/o a tiempo parcial",
-    Autonomo = "Autónoma/o",
-    Estudiante = "Estudiante",
-    Otro = "Otro"
+    sinIngresos = "Desempleada/o sin ingresos",
+    subsidio = "Desempleada/o con subsidio por desempleo",
+    tiempoCompleto = "Empleada/o a tiempo completo",
+    tiempoParcial = "Empleada/o a tiempo parcial",
+    autonomo = "Autónoma/o",
+    estudiante = "Estudiante",
+    otro = "Otro"
 }
 
 export enum TipoInteresesActuales {
-    CompetenciasTecnologicas = "Estoy interesada/o en formarme en competencias tecnológicas",
-    EmpleoTecnologico = "Estoy interesada/o en obtener un empleo en el sector tecnológico",
-    EmpleoEstable = "Necesito formarme para conseguir un empleo estable",
-    CambiarSector = "Quiero cambiar de sector profesional",
-    Otro = "Otra"
+    competenciasTecnologicas = "Estoy interesada/o en formarme en competencias tecnológicas",
+    empleoTecnologico = "Estoy interesada/o en obtener un empleo en el sector tecnológico",
+    empleoEstable = "Necesito formarme para conseguir un empleo estable",
+    cambiarSector = "Quiero cambiar de sector profesional",
+    otro = "Otra"
 }
 
 export enum TipoAccessoInternetDispositivos {
-    SinAcceso = "Sin acceso (no tengo conexión a internet ni dispositivo)",
-    MuyLimitado = "Acceso muy limitado (tengo dispositivo pero no tengo conexión a internet)",
-    Limitado = "Acceso limitado (tengo dispositivo y conexión a internet limitada)",
-    Medio = "Acesso medio (tengo dispositivo y conexión a internet, a pesar de no ser de buena calidad)",
-    Buen = "Buen acceso (tengo buena conexión a internet y dispositivo)"
+    sinAcceso = "Sin acceso (no tengo conexión a internet ni dispositivo)",
+    muyLimitado = "Acceso muy limitado (tengo dispositivo pero no tengo conexión a internet)",
+    limitado = "Acceso limitado (tengo dispositivo y conexión a internet limitada)",
+    medio = "Acesso medio (tengo dispositivo y conexión a internet, a pesar de no ser de buena calidad)",
+    buen = "Buen acceso (tengo buena conexión a internet y dispositivo)"
 }
 
 export enum TipoEncontrarPrograma {
-    RedesSociales = "Redes sociales",
+    redesSociales = "Redes sociales",
     SomosF5 = "Somos F5",
     FactoriaF5 = "Factoría F5",
     FundacionDonBosco = "Fundación Don Bosco o sus redes sociales",
-    Buscadores = "Buscadores de internet (Google)"
+    buscadores = "Buscadores de internet (Google)"
 }
