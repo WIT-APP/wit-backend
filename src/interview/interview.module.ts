359c41747a27e5daaaa9be56b2bfacddbeef6558
tito <jfctito@gmail.com>
2023-09-27 14:31:01 +0200

    setting module for interview to inject entity

--- src/interview/interview.module.ts
+++ src/interview/interview.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { InterviewService } from './interview.service';
 import { InterviewController } from './interview.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Interview from './entities/interview.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Interview])],
   providers: [InterviewService],
-  controllers: [InterviewController]
+  controllers: [InterviewController],
 })
 export class InterviewModule {}
