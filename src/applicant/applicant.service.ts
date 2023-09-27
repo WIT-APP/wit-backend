19f57da6528a54af1eecd2a87120d0c08a2a3445
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-09-27 14:56:28 +0200

    error handling simplified

--- src/applicant/applicant.service.ts
+++ src/applicant/applicant.service.ts
@@ -1,15 +1,13 @@
 import {
-  ConflictException,
-  ForbiddenException,
+  BadRequestException,
   HttpException,
   HttpStatus,
   Injectable,
-  InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
 import { CreateApplicantDto } from './dto/create-applicant.dto';
 import { UpdateApplicantDto } from './dto/update-applicant.dto';
-import { EntityManager, In, Repository, getManager } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Applicant } from './entities/applicant.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -21,32 +19,37 @@ export class ApplicantService {
   ) {}
 
   async create(createApplicantDto: CreateApplicantDto): Promise<Applicant> {
-  
-      return await this.applicantRepository.save(createApplicantDto);
-   
+    const newApplicant = await this.applicantRepository.save(
+      createApplicantDto,
+    );
+
+    if (!newApplicant) {
+      throw new BadRequestException();
+    }
+
+    return newApplicant;
   }
 
   async getDuplicateEmails(): Promise<any[]> {
-    try {
-      const queryResult = await this.applicantRepository
-        .createQueryBuilder('a1')
-        .select(['a1.*'])
-        .addSelect('a1.correo_electronico')
-        .innerJoin(
-          'applicant',
-          'a2',
-          'a1.id <> a2.id AND a1.correo_electronico = a2.correo_electronico',
-        )
-        .groupBy('a1.correo_electronico, a1.id')
-        .orderBy('a1.fecha_de_applicacion', 'DESC')
-        .getRawMany();
+    const queryResult = await this.applicantRepository
+      .createQueryBuilder('a1')
+      .select(['a1.*'])
+      .addSelect('a1.correo_electronico')
+      .innerJoin(
+        'applicant',
+        'a2',
+        'a1.id <> a2.id AND a1.correo_electronico = a2.correo_electronico',
+      )
+      .groupBy('a1.correo_electronico, a1.id')
+      .orderBy('a1.fecha_de_applicacion', 'DESC')
+      .getRawMany();
 
-      return queryResult;
-    } catch (error) {
-      throw new Error(
+    if (!queryResult) {
+      throw new BadRequestException(
         'Error al recuperar los correos electrónicos duplicados.',
       );
     }
+    return queryResult;
   }
 
   async getUsersPreapproved(): Promise<Applicant[]> {
@@ -57,15 +60,14 @@ export class ApplicantService {
     HAVING COUNT(correo_electronico) > 1
   `;
 
-    try {
-      const nonUniqueEmailsResult = await this.applicantRepository.query(
-        nonUniqueEmailsQuery,
-      );
-      const nonUniqueEmails = nonUniqueEmailsResult.map(
-        (result) => result.correo_electronico,
-      );
+    const nonUniqueEmailsResult = await this.applicantRepository.query(
+      nonUniqueEmailsQuery,
+    );
+    const nonUniqueEmails = nonUniqueEmailsResult.map(
+      (result) => result.correo_electronico,
+    );
 
-      const updateQuery = `
+    const updateQuery = `
       UPDATE applicant
       SET estado = 'Preaprovado'
       WHERE correo_electronico NOT IN (${nonUniqueEmails
@@ -75,11 +77,9 @@ export class ApplicantService {
       AND estado = 'Aplicante'
       RETURNING *;
     `;
-      const updatedApplicants = await this.applicantRepository.query(
-        updateQuery,
-      );
+    const updatedApplicants = await this.applicantRepository.query(updateQuery);
 
-      const preaprovadoQuery = `
+    const preaprovadoQuery = `
       SELECT *
       FROM applicant
       WHERE correo_electronico NOT IN (${nonUniqueEmails
@@ -88,44 +88,36 @@ export class ApplicantService {
       AND pais_de_residencia = 'España'
       AND estado = 'Preaprovado';
     `;
-      const preaprovadoApplicants = await this.applicantRepository.query(
-        preaprovadoQuery,
-      );
+    const preaprovadoApplicants = await this.applicantRepository.query(
+      preaprovadoQuery,
+    );
 
-      return preaprovadoApplicants;
-    } catch (error) {
-      throw new Error('Error al recuperar usuarios preaprobados.');
+    if (!preaprovadoApplicants) {
+      throw new NotFoundException('No se encontraron personas en este estado.');
     }
+
+    return preaprovadoApplicants;
   }
 
   async findAll(): Promise<Applicant[]> {
-    try {
-      return await this.applicantRepository.find();
-    } catch (error) {
-      throw new ForbiddenException();
+    const applicants = await this.applicantRepository.find();
+
+    if (!applicants) {
+      throw new NotFoundException();
     }
+    return applicants;
   }
 
   async findOneById(id: number): Promise<Applicant> {
-    try {
-      const applicant = await this.applicantRepository.findOne({
-        where: { id },
-      });
+    const applicant = await this.applicantRepository.findOne({
+      where: { id },
+    });
 
-      if (!applicant) {
-        throw new NotFoundException('Usuario no encontrado');
-      }
-
-      return applicant;
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        throw new NotFoundException('Usuario no encontrado');
-      } else {
-        throw new InternalServerErrorException(
-          'Error al recuperar el solicitante',
-        );
-      }
+    if (!applicant) {
+      throw new NotFoundException('Aspirante no encontrado');
     }
+
+    return applicant;
   }
 
   async findByEmail(email: string): Promise<Applicant> {
@@ -152,7 +144,7 @@ export class ApplicantService {
         'No se encontraron personas en este estado.',
         HttpStatus.NOT_FOUND,
       );
-    } 
+    }
 
     return applicants;
   }
@@ -172,7 +164,6 @@ export class ApplicantService {
     return applicants;
   }
 
-
   async updateEstado(
     id: number,
     updateApplicantDto: UpdateApplicantDto,
@@ -183,16 +174,10 @@ export class ApplicantService {
       throw new NotFoundException('No se encontraron el aspirante.');
     }
 
-    try {
-      applicant.estado = updateApplicantDto.estado;
+    applicant.estado = updateApplicantDto.estado;
 
-      const updatedApplicant = await this.applicantRepository.save(applicant);
-      return updatedApplicant;
-    } catch (error) {
-      throw new InternalServerErrorException(
-        'Occurrió un error actualizando el aspirante.',
-      );
-    }
+    const updatedApplicant = await this.applicantRepository.save(applicant);
+    return updatedApplicant;
   }
 
   async updateApplicant(
@@ -204,49 +189,42 @@ export class ApplicantService {
       throw new NotFoundException('No se encontraron el aspirante.');
     }
 
-    try {
-      applicant.nombre = updateApplicantDto.nombre;
-      applicant.apellidos = updateApplicantDto.apellidos;
-      applicant.correo_electronico = updateApplicantDto.correo_electronico;
-      applicant.telefono = updateApplicantDto.telefono;
-      applicant.estado = updateApplicantDto.estado;
-      applicant.genero = updateApplicantDto.genero;
-      applicant.fecha_de_nacimiento = updateApplicantDto.fecha_de_nacimiento;
-      applicant.pais_de_nacimiento = updateApplicantDto.pais_de_nacimiento;
-      applicant.documento_de_identidad =
-        updateApplicantDto.documento_de_identidad;
-      applicant.tipo_documento_identidad =
-        updateApplicantDto.tipo_documento_identidad;
-      applicant.numero_documento_id = updateApplicantDto.numero_documento_id;
-      applicant.direccion = updateApplicantDto.direccion;
-      applicant.ciudad = updateApplicantDto.ciudad;
-      applicant.provincia = updateApplicantDto.provincia;
-      applicant.codigo_postal = updateApplicantDto.codigo_postal;
-      applicant.pais_de_residencia = updateApplicantDto.pais_de_residencia;
-      applicant.programa_cursar = updateApplicantDto.programa_cursar;
-      applicant.permiso = updateApplicantDto.permiso;
-      applicant.colectivo = updateApplicantDto.colectivo;
-      applicant.educacion = updateApplicantDto.educacion;
-      applicant.estudio_mas_alto = updateApplicantDto.estudio_mas_alto;
-      applicant.situacion_profesional =
-        updateApplicantDto.situacion_profesional;
-      applicant.intereses_actuales = updateApplicantDto.intereses_actuales;
-      applicant.dedicacion_semanal = updateApplicantDto.dedicacion_semanal;
-      applicant.acceso_internet_dispositivos =
-        updateApplicantDto.acceso_internet_dispositivos;
-      applicant.formacion_online = updateApplicantDto.formacion_online;
-      applicant.razones_para_unir = updateApplicantDto.razones_para_unir;
-      applicant.encontrar_programa = updateApplicantDto.encontrar_programa;
-      applicant.mas_informacion = updateApplicantDto.mas_informacion;
-      applicant.observaciones = updateApplicantDto.observaciones;
+    applicant.nombre = updateApplicantDto.nombre;
+    applicant.apellidos = updateApplicantDto.apellidos;
+    applicant.correo_electronico = updateApplicantDto.correo_electronico;
+    applicant.telefono = updateApplicantDto.telefono;
+    applicant.estado = updateApplicantDto.estado;
+    applicant.genero = updateApplicantDto.genero;
+    applicant.fecha_de_nacimiento = updateApplicantDto.fecha_de_nacimiento;
+    applicant.pais_de_nacimiento = updateApplicantDto.pais_de_nacimiento;
+    applicant.documento_de_identidad =
+      updateApplicantDto.documento_de_identidad;
+    applicant.tipo_documento_identidad =
+      updateApplicantDto.tipo_documento_identidad;
+    applicant.numero_documento_id = updateApplicantDto.numero_documento_id;
+    applicant.direccion = updateApplicantDto.direccion;
+    applicant.ciudad = updateApplicantDto.ciudad;
+    applicant.provincia = updateApplicantDto.provincia;
+    applicant.codigo_postal = updateApplicantDto.codigo_postal;
+    applicant.pais_de_residencia = updateApplicantDto.pais_de_residencia;
+    applicant.programa_cursar = updateApplicantDto.programa_cursar;
+    applicant.permiso = updateApplicantDto.permiso;
+    applicant.colectivo = updateApplicantDto.colectivo;
+    applicant.educacion = updateApplicantDto.educacion;
+    applicant.estudio_mas_alto = updateApplicantDto.estudio_mas_alto;
+    applicant.situacion_profesional = updateApplicantDto.situacion_profesional;
+    applicant.intereses_actuales = updateApplicantDto.intereses_actuales;
+    applicant.dedicacion_semanal = updateApplicantDto.dedicacion_semanal;
+    applicant.acceso_internet_dispositivos =
+      updateApplicantDto.acceso_internet_dispositivos;
+    applicant.formacion_online = updateApplicantDto.formacion_online;
+    applicant.razones_para_unir = updateApplicantDto.razones_para_unir;
+    applicant.encontrar_programa = updateApplicantDto.encontrar_programa;
+    applicant.mas_informacion = updateApplicantDto.mas_informacion;
+    applicant.observaciones = updateApplicantDto.observaciones;
 
-      const updatedApplicant = await this.applicantRepository.save(applicant);
-      return updatedApplicant;
-    } catch (error) {
-      throw new InternalServerErrorException(
-        'Occurrió un error actualizando el aspirante.',
-      );
-    }
+    const updatedApplicant = await this.applicantRepository.save(applicant);
+    return updatedApplicant;
   }
 
   remove(id: number) {
