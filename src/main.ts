9ebbfa19e9e6a1293eacab4c685d35828aeac51c
Angela-GM <angelagm0112@gmail.com>
2023-10-03 15:56:49 +0200

    #4 añadir url frontend

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalFilters(new HttpExceptionFilter());
 	app.enableCors({
-		allowedHeaders: ["content-type"],
-		origin: ["http://localhost:5173", process.env.FRONTEND_URL],
-		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-		credentials: true,
+		// allowedHeaders: ["content-type"],
+		// origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+		// methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		// credentials: true,
 	});
 	await app.listen(process.env.PORT || 3000);
 }
