8d3a6657432e9538dc067c6c17a0cc68080c043c
Angela-GM <angelagm0112@gmail.com>
2023-10-03 16:45:04 +0200

    prueba con *

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,15 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: [
-			"https://wit-frontend-1v1pqlfbf-wit-app.vercel.app",
-			"https://wit-frontend-git-main-wit-app.vercel.app",
-			"https://wit-frontend-green.vercel.app",
-			"https://wit-frontend-1v1pqlfbf-wit-app.vercel.app/newapplicant/register",
-			"https://wit-frontend-git-main-wit-app.vercel.app7newapplicant/register",
-			"https://wit-frontend-green.vercel.app/newapplicant/register",
-		],
+		// origin: [
+		// 	"https://wit-frontend-1v1pqlfbf-wit-app.vercel.app",
+		// 	"https://wit-frontend-git-main-wit-app.vercel.app",
+		// 	"https://wit-frontend-green.vercel.app",
+		// 	"https://wit-frontend-1v1pqlfbf-wit-app.vercel.app/newapplicant/register",
+		// 	"https://wit-frontend-git-main-wit-app.vercel.app7newapplicant/register",
+		// 	"https://wit-frontend-green.vercel.app/newapplicant/register",
+		// ],
+		origin: "*",
 		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 		credentials: true,
 		allowedHeaders: "Content-Type,Authorization",
