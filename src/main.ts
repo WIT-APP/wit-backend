741cf9835d23f94c4eb320ba53140632222b2da2
Angela-GM <angelagm0112@gmail.com>
2023-10-03 16:33:26 +0200

    prueba con las 3 urls

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: "https://wit-frontend-1v1pqlfbf-wit-app.vercel.app",
+		origin: [
+			"https://wit-frontend-1v1pqlfbf-wit-app.vercel.app",
+			"https://wit-frontend-git-main-wit-app.vercel.app",
+			"https://wit-frontend-green.vercel.app",
+			// Agrega aquí todas las URL que deseas permitir
+		],
 		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 		credentials: true, // Si necesitas admitir credenciales
 		allowedHeaders: "Content-Type,Authorization",
