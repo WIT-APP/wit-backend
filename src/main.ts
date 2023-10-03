295ca1fe8df288135c42e8bc4dde6d39263fa18d
Angela-GM <angelagm0112@gmail.com>
2023-10-03 16:26:46 +0200

    prueba con variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { HttpExceptionFilter } from "./exception-filters/http-exception.filter";
+// import { HttpExceptionFilter } from "./exception-filters/http-exception.filter";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.useGlobalFilters(new HttpExceptionFilter());
 	app.enableCors({
-		origin: "https://wit-frontend-1v1pqlfbf-wit-app.vercel.app",
-		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-		allowedHeaders: "Content-Type,Authorization",
+		allowedHeaders: ["content-type"],
+		origin: [
+			process.env.FRONTEND_URL,
+			"http://localhost:3000",
+			"http://localhost:5173",
+		],
+		credentials: true,
 	});
 	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
-
