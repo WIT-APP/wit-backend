f408c3a69d795558c9f9c192f64c67bd9ba221e8
Angela-GM <angelagm0112@gmail.com>
2023-10-03 16:37:20 +0200

    url register

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-// import { HttpExceptionFilter } from "./exception-filters/http-exception.filter";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -9,10 +8,10 @@ async function bootstrap() {
 			"https://wit-frontend-1v1pqlfbf-wit-app.vercel.app",
 			"https://wit-frontend-git-main-wit-app.vercel.app",
 			"https://wit-frontend-green.vercel.app",
-			// Agrega aquí todas las URL que deseas permitir
+			"https://wit-frontend-1v1pqlfbf-wit-app.vercel.app/newapplicant/register"
 		],
 		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-		credentials: true, // Si necesitas admitir credenciales
+		credentials: true,
 		allowedHeaders: "Content-Type,Authorization",
 	});
 	await app.listen(process.env.PORT || 3000);
