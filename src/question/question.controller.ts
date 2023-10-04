a2624cc756d71aa5fb6d9ba0e20e9acfc165c65a
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-10-04 15:25:58 +0200

    update

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -14,13 +14,13 @@ export class QuestionController {
 	async create(@Body() createQuestionDto: CreateQuestionDto) {
 		return await this.questionService.create(createQuestionDto);
 	}
-
+	
+	@Public()
 	@Get()
 	findByCategory(@Query("category") category: TypeCategory) {
 		return this.questionService.findByCategory(category);
 	}
-	
-	@Public()
+
 	@Get()
 	findAll() {
 		return this.questionService.findAll();
